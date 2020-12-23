b356c9b45371787457b3a499bec8cbb0e0a5f406
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2020-12-23 22:37:02 +0900

    Add dotenv-load functions.
    Fixed docstrings.

--- dotenv-mode.el
+++ dotenv-mode.el
@@ -124,6 +124,91 @@
         "\\.env\\.example\\'"
         ))
 
+
+(defvar dotenv-definition-line-rx
+  (rx line-start
+
+      (group (+ (or alpha "_" num)))
+      "="
+      (group (+ printing))
+      line-end)
+  "Regexp for dotenv style line.")
+
+
+(defvar dotenv-file-path nil
+  "Path to dotenv file.")
+
+(defvar dotenv-original-process-environment (copy-sequence process-environment)
+  "Original process environment variables.
+
+This value is applied when reading another Dotenv file.
+Therefore, the value of Dotenv does not continue to be set.")
+
+
+(defvar dotenv-definition-line-max-position 1000000000
+  "Maximum buffer size for Dotenv files.
+
+Define it as insurance when a huge dotenv is read.")
+
+(defun dotenv-trim-value (value-definition)
+  "Trim the value part of dotenv.
+
+Whitespace is processed and comments are removed."
+  (string-match (rx line-start (group (+ (not "#"))) (? "#" (* anything)) line-end)
+		value-definition)
+  (s-trim (match-string 1 value-definition)))
+
+
+(defun dotenv-format-entry (name-value)
+  "Format one line of dotenv into a format that can be handled by process-environment.
+
+Example::
+  FOO=BAR"
+  (let ((name (car name-value))
+	(value (cdr name-value)))
+    (format "%s=%s"
+	    (string-as-unibyte (car name-value))
+	    (dotenv-trim-value
+	     (string-as-unibyte (cdr name-value))))))
+
+
+(defun dotenv-search-forward-definition ()
+  "Finds the dotenv definition from the current point position in the current buffer and returns it as a list of cons"
+  (if-let ((pos (re-search-forward dotenv-definition-line-rx
+				   dotenv-definition-line-max-position
+				   t)))  ;; no error
+      (cons
+       (cons (match-string 1)  ;; name
+	     (match-string 2)) ;; value
+       (dotenv-search-forward-definition))))
+
+
+(defun dotenv-get-definitions-from-buffer ()
+  "Finds and returns the dotenv definition in the current buffer"
+  (save-excursion
+    (goto-char 0)
+    (dotenv-search-forward-definition)))
+
+
+(defmacro dotenv-with-open-file (file-path &rest body)
+  "Finds and returns the dotenv definition in the specified file"
+  `(let* ((buf (find-file-noselect ,file-path))
+	  (resp (with-current-buffer buf
+		  ,@body)))
+     (kill-buffer buf)
+     resp))
+
+
+(defun dotenv-load ()
+  (interactive)
+  (let* ((buf (find-file-noselect dotenv-file-path))
+	 (envs (with-current-buffer buf (dotenv-get-definitions-from-buffer))))
+    (kill-buffer buf)
+
+    (setq process-environment
+	  (append envs dotenv-original-process-environment))))
+
+
 (provide 'dotenv-mode)
 
 ;;; dotenv-mode.el ends here
